d66e9b0eeee51db0c6a3102abd07580a98c807c4
SENAN111 <behbudovsenan0@gmail.com>
2022-11-11 23:11:06 +0400

    html

---
+++ task.js
@@ -0,0 +1,14 @@
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const age=  document.getElementById("age");
+const experience =  document.getElementById("experience");
+const nationality =  document.getElementById("Natinality");
+const Position =  document.getElementById("Position");
+const myList = document.createElement("table");
+const bodyElement = document.getElementsByTagName("body")[0];
+bodyElement.appendChild(table);
+const fname1 = document.getElementById("fname")[0];
+const lname1 = document.getElementById("lname")[0];
+const age1= document.getElementById("age")[0];
+
+ 
